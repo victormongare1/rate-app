0fd4c59a5dd60332b085a58cbfababef2de3e5be
victormongare1 <vicmongz254@gmail.com>
2020-06-05 19:52:30 +0300

    created class for profile

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    profile class to define profile objects
+    '''
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic=models.ImageField(upload_to = 'images/')
+    bio=models.CharField(max_length = 100)
+    contacts=models.CharField(max_length = 60)
