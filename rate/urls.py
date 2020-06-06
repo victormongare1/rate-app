ebf19dd8a682358a6ebdad9b823413d7506a27de
victormongare1 <vicmongz254@gmail.com>
2020-06-06 11:22:13 +0300

    created url patterm for homepage

--- rate/urls.py
+++ rate/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-  
+    url(r'^$',views.home,name = 'home'),
     url(r'^search/',views.search_results, name='search_results'),
     url(r'^api/profiles/$',views.ProfileList.as_view()),
     url(r'^api/projects/$',views.ProjectList.as_view()),
