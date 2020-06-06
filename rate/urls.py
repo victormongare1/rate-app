0d61d43682298d6dee33e325cc940d6169dd22bc
victormongare1 <vicmongz254@gmail.com>
2020-06-06 12:42:16 +0300

    created url pattern for creating a profile

--- rate/urls.py
+++ rate/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
     url(r'^$',views.home,name = 'home'),
     url(r'^search/',views.search_results, name='search_results'),
     url(r'^profile/(\d+)',views.profile,name = 'profile'),
+    url(r'^createprofile',views.create_profile,name="create_profile"),
     url(r'^accounts/profile',views.profile,name='profile'),
     url(r'^api/profiles/$',views.ProfileList.as_view()),
     url(r'^api/projects/$',views.ProjectList.as_view()),
