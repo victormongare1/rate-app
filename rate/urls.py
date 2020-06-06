dd60ed24b90f6921836adecb52caf7f204d905bb
victormongare1 <vicmongz254@gmail.com>
2020-06-06 11:19:02 +0300

    created url pattern for profile api

--- rate/urls.py
+++ rate/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-
+    url(r'^api/profiles/$',views.ProfileList.as_view()),
+    ur
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
