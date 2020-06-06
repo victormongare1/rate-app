5003af32b8f4b1e11bfbe99662f39f4fe18353f6
victormongare1 <vicmongz254@gmail.com>
2020-06-06 12:10:42 +0300

    created test class for project class

--- rate/tests.py
+++ rate/tests.py
@@ -19,3 +19,20 @@ class ProfileTestClass(TestCase):
         profiles= Profile.objects.all()
         self.assertTrue(len(profiles) == 0)
 
+class ProjectTestClass(TestCase):
+    '''
+    test class to test methods of the  Project class
+    '''
+    def setUp(self):
+        self.profile=Profile(user="victor",profile_pic="pic",bio="person",contacts="vicmongz254@gmail.com")
+        self.image=Image(image="image",name="image",caption="nice",profile=self.profile)
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0) 
+
+    def test_delete_method(self):L
+        self.image.delete_image()
+        images=Image.objects.all()
+        self.assertTrue(len(images) > 0)        
+
