aadc99992bc0c1d6053798d8dc2a8fb8c111b609
victormongare1 <vicmongz254@gmail.com>
2020-06-06 11:10:44 +0300

    created serializer class for project objects

--- rate/serializer.py
+++ rate/serializer.py
@@ -5,3 +5,8 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('id','user', 'profile_pic', 'bio','contacts')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id','title','description','image','link','profile')        
