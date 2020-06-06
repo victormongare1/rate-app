fa0972ce0c95e0ec5bb7f57f3cfc3f52b2bccac5
victormongare1 <vicmongz254@gmail.com>
2020-06-06 10:48:46 +0300

    added view route for homepage

--- rate/views.py
+++ rate/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Profile,Project,Review
+from django.contrib.auth.models import User
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home():
+    '''
+    view route for home page
+    '''
+    projects=Project.objects.all()
+    return render(request,{'projects':projects})
+
+@login_required(login_url='/accounts/login/')
+    
