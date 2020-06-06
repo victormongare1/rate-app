917d913b147df485fd00a1ea042047e2473f2fe6
victormongare1 <vicmongz254@gmail.com>
2020-06-06 11:16:51 +0300

    created view route for project serializer

--- rate/views.py
+++ rate/views.py
@@ -32,4 +32,8 @@ class ProfileList(APIView):
         serializers = ProfileSerializer(all_profiles, many=True)
         return Response(serializers.data)
 
-        
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)        
