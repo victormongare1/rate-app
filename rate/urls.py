27b85d520313c2cce37fd78ce03f5f53b4ee09f3
victormongare1 <vicmongz254@gmail.com>
2020-06-06 11:20:05 +0300

    created url patterm for projects api

--- rate/urls.py
+++ rate/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
+    
     url(r'^api/profiles/$',views.ProfileList.as_view()),
-    ur
+    url(r'^api/projects/$',views.ProjectList.as_view()),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
