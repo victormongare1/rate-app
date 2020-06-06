41c7a6f38e7aad06354ef8d57178d7b73f9c21ee
victormongare1 <vicmongz254@gmail.com>
2020-06-06 12:41:34 +0300

    created url pattern for existing profiles

--- rate/urls.py
+++ rate/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.home,name = 'home'),
     url(r'^search/',views.search_results, name='search_results'),
+    url(r'^profile/(\d+)',views.profile,name = 'profile'),
+    url(r'^accounts/profile',views.profile,name='profile'),
     url(r'^api/profiles/$',views.ProfileList.as_view()),
     url(r'^api/projects/$',views.ProjectList.as_view()),
 ]
