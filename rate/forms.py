2a4210e4769f7094c2f47ad88f837a630fec63de
victormongare1 <vicmongz254@gmail.com>
2020-06-06 12:01:07 +0300

    created form for creating reviews and ratings

--- rate/forms.py
+++ rate/forms.py
@@ -17,4 +17,13 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exlcude = ['user']
-        fields = ('title','description','link','image')        
+        fields = ('title','description','link','image') 
+
+class ReviewForm(forms.ModelForm):
+    '''
+    class to define Review form
+    '''
+    class Meta:
+        model = Profile
+        exlcude = ['user','project']
+        fields = ('comment','design','usability','content')               
